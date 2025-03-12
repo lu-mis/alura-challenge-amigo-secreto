ebfc9fe2f87bc6bfaf80e3e11095124ea64f1fc9
Luis Fernando <luiz-fernando-rios@outlook.com>
2025-03-12 15:39:24 -0300

    recebendo nomes

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let amigos = []
+
+function adicionarAmigo() {
+    amigos = document.querySelector('input').value;
+    console.log(amigos);
+}
